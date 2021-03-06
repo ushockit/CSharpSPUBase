1908e109cf75e4f558d39b44f28957175a227ba9
ushock <ushock.it@gmail.com>
2021-03-06 11:40:36 +0200

    Add Lesson 01

---
+++ Lesson 01/Lesson01/Example/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        const int SIZE = 4;
+        const int ZERO = 0;
+        static string username;
+        static int[,] gameField = new int[SIZE, SIZE];
+        static Random rnd = new Random();
+        //позиция 0 на поле
+        static int emptyCellRow;
+        static int emptyCellCol;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            username = "Vasya";
+            //username = Console.ReadLine();
+
+            Start();
+
+            Console.Read();
+        }
+
+        static void Start()
+        {
+            //инициализация
+            Init();
+            //перемешивание массива
+            Shuffle();
+
+            while(true)
+            {
+                //очистка консоли
+                Console.Clear();
+                //текст приветствия пользователя
+                WelcomeText();
+                //вывод поля
+                DrawField();
+
+                ConsoleKeyInfo key = Console.ReadKey();
+
+                //обработка нажатой клавиши
+                ProccessKey(key.Key);
+            }
+        }
+
+        static void WelcomeText()
+        {
+            Console.WriteLine($"Hello, {username}");
+        }
+
+        static void ProccessKey(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.UpArrow:
+                    MoveToUp();
+                    break;
+                case ConsoleKey.DownArrow:
+                    MoveToDown();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    MoveToLeft();
+                    break;
+                case ConsoleKey.RightArrow:
+                    MoveToRight();
+                    break;
+            }
+        }
+
+        static void MoveToRight()
+        {
+            if (emptyCellCol > 0)
+            {
+                int tmp = gameField[emptyCellRow, emptyCellCol - 1];
+                gameField[emptyCellRow, emptyCellCol - 1] = ZERO;
+                gameField[emptyCellRow, emptyCellCol] = tmp;
+                emptyCellCol--;
+            }
+        }
+
+        static void MoveToLeft()
+        {
+            if (emptyCellCol < SIZE - 1)
+            {
+                int tmp = gameField[emptyCellRow, emptyCellCol + 1];
+                gameField[emptyCellRow, emptyCellCol + 1] = ZERO;
+                gameField[emptyCellRow, emptyCellCol] = tmp;
+                emptyCellCol++;
+            }
+        }
+
+        static void MoveToDown()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void MoveToUp()
+        {
+            throw new NotImplementedException();
+        }
+
+        static void DrawField()
+        {
+            for (int i = 0; i < gameField.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameField.GetLength(1); j++)
+                {
+                    if (gameField[i, j] != ZERO)
+                        Console.Write($"{gameField[i, j], 3}");
+                    else
+                        Console.Write($"{" ", 3}");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Init()
+        {
+            //инициализация позиции нуля
+            emptyCellCol = emptyCellRow = SIZE - 1;
+
+            int counter = 1;
+            for (int i = 0; i < gameField.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameField.GetLength(1); j++)
+                {
+                    gameField[i, j] = counter++;
+                }
+            }
+            gameField[SIZE - 1, SIZE - 1] = ZERO;
+        }
+
+        static void Shuffle()
+        {
+
+            for (int i = 0; i < gameField.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameField.GetLength(1); j++)
+                {
+                    int i1 = rnd.Next(0, SIZE);
+                    int j1 = rnd.Next(0, SIZE);
+                    int i2 = rnd.Next(0, SIZE);
+                    int j2 = rnd.Next(0, SIZE);
+
+                    int tmp = gameField[i1, j1];
+                    gameField[i1, j1] = gameField[i2, j2];
+                    gameField[i2, j2] = tmp;
+                }
+            }
+            MoveZeroToLastCell();
+        }
+
+        static void MoveZeroToLastCell()
+        {
+            for (int i = 0; i < gameField.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameField.GetLength(1); j++)
+                {
+                    if (gameField[i, j] == ZERO)
+                    {
+                        gameField[i, j] = gameField[SIZE - 1, SIZE - 1];
+                        gameField[SIZE - 1, SIZE - 1] = ZERO;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lesson 01/Lesson01/Lesson01/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //вывод на консоль без перевода строки
+            Console.Write("Hello");
+            Console.Write(" world!!!");
+
+            //вывод на консоль с переводом строки
+            Console.WriteLine();
+            Console.WriteLine("Hello");
+            Console.WriteLine(" world!!!");
+
+            Console.WriteLine("Hello" + " world");
+
+
+            int a = 10;
+            bool _bool = true;
+            char _char = 'z';
+            double _double = 5.35;
+            decimal _decimal = 10.8m;
+            string _string = "Hello";
+
+
+            Console.WriteLine("a = " + a);
+
+            //упрощение экранирования
+            Console.WriteLine(@"C:\Program Files\Some Folder");
+
+            //интерполяция
+            Console.WriteLine($"Value - {a}");
+            string name = "Vasya";
+            Console.WriteLine($"Hello, {name}");
+
+            string s1 = "Hello world";
+
+            // -= Ввод данных с клавиатуры =-
+            /*Console.Write("Enter your string: ");
+            //ожидание ввода строки
+            string str = Console.ReadLine();
+            Console.WriteLine($"Your string '{str}'");*/
+
+            //Console.Write("Enter symb: ");
+            //ConsoleKeyInfo key = Console.ReadKey();
+            //if (key.Key == ConsoleKey.G)
+            //{
+            //    Console.WriteLine("Hello C#");
+            //}
+            //Console.WriteLine($"Symb '{key.KeyChar}'");
+
+
+            /*if (key.Key == ConsoleKey.S &&
+                key.Modifiers == ConsoleModifiers.Control)
+            {
+                Console.WriteLine("Ctrl + S");
+            }*/
+
+
+            /*string str = Console.ReadLine();
+            //преобразование из строки в число
+            int b = int.Parse(str);*/
+            //int b = int.Parse(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+
+            //string[] words = new string[] { "Word 1", "Word 2", "Word 3", "Word 4", "Word 5" };
+
+            //for (int i = 0; i < words.Length; i++)
+            //{
+            //    Console.WriteLine(words[i]);
+            //}
+
+            /*foreach (string w in words)
+            {
+                Console.WriteLine(w);
+            }*/
+
+            /*string str = " Hello world";
+            Console.WriteLine(str);
+            Console.WriteLine($"Length = {str.Length}");
+            Console.WriteLine($"Substring = {str.Substring(2, 3)}");
+            Console.WriteLine($"Trim = {str.Trim()}");
+            //строка начинается с пробела
+            if (str.StartsWith(" ")) 
+            {
+                Console.WriteLine("True");
+            }*/
+            string str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
+            //разбиение строки на массив слов
+            string[] words = str.Split(new char[] { ' ', '.', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            /*int[] nums = new int[10];
+            //класс для генерирования случайных чисел
+            Random rnd = new Random();
+
+            //заполнение массива случайными числами
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = rnd.Next(10, 100);
+                Console.WriteLine(nums[i]);
+            }*/
+
+            Random rnd = new Random();
+            //объявление двумерного массива
+            /*int[,] dArr = new int[5, 7];
+            Console.WriteLine($"Total size = {dArr.Length}");
+            Console.WriteLine($"Rows = {dArr.GetLength(0)}");
+            Console.WriteLine($"Cols = {dArr.GetLength(1)}");
+
+            for (int i = 0; i < dArr.GetLength(0); i++)
+            {
+                for (int j = 0; j < dArr.GetLength(1); j++)
+                {
+                    dArr[i, j] = rnd.Next(10, 20);
+                    Console.Write($"{dArr[i, j]} ");
+                }
+                Console.WriteLine();
+            }*/
+
+            //Двумерный рваный массив
+            /*int[][] dArr = new int[10][];
+
+            for (int i = 0; i < dArr.GetLength(0); i++)
+            {
+                //создание массива с случайным кол-вом ячеек
+                dArr[i] = new int[rnd.Next(5, 15)];
+                for (int j = 0; j < dArr[i].Length; j++)
+                {
+                    dArr[i][j] = rnd.Next(10, 20);
+                    Console.Write($"{dArr[i][j]} ");
+                }
+                Console.WriteLine();
+            }*/
+
+
+
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(10, 20);
+            }
+
+            int[] arr2 = new int[arr.Length];
+
+            //копирование одного массива в другой
+            Array.Copy(arr, arr2, arr.Length);
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
